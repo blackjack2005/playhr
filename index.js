0e079126a9a6d8202d0564d489b1ce7fe5809b54
Alex Huang <alex_huang@wistron.com>
2021-10-07 22:49:37 +0800

    How to wait properly?

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
+const assert = require('assert');
 
 function idle(ms) {
   return new Promise(resolve => setTimeout(() => resolve(), ms));
@@ -42,36 +43,62 @@ const keypress = () => {
   await page.goto("https://hr.wistron.com/psp/PRD/EMPLOYEE/HRMS/c/ROLE_MANAGER.GP_ABS_MGRSS_HIST.GBL?NAVSTACK=Clear&PORTALPARAM_PTCNAV=HC_GP_ABS_MGRSS_HIST_GBL&EOPP.SCNode=HRMS&EOPP.SCPortal=EMPLOYEE&EOPP.SCName=ADMN_MANAGER_REVIEWS&EOPP.SCLabel=%e9%83%a8%e5%b1%9e%e7%94%b3%e8%af%b7%e8%ae%b0%e5%bd%95%e6%9f%a5%e8%af%a2&EOPP.SCFName=ADMN_F201512302128141443830683&EOPP.SCSecondary=true&EOPP.SCPTcname=PT_PTPP_SCFNAV_BASEPAGE_SCR&FolderPath=PORTAL_ROOT_OBJECT.CO_MANAGER_SELF_SERVICE.HC_TIME_MANAGEMENT.HC_VIEW_TIME_MGR.HC_GP_ABS_MGRSS_HIST_GBL&IsFolder=false");
 
   async function GetLeaveRecords(did, eid, employeeName) {
-    await page.frame({name: 'TargetContent'}).fill('input[id="DERIVED_ABS_SS_BGN_DT"]', '2020/01/01');
-    await page.frame({name: 'TargetContent'}).fill('input[id="DERIVED_ABS_SS_END_DT"]', '2020/8/1');
+    await page.frame({name: 'TargetContent'}).fill('input[id="DERIVED_ABS_SS_BGN_DT"]', '2020/1/1');
+    await page.frame({name: 'TargetContent'}).fill('input[id="DERIVED_ABS_SS_END_DT"]', '2021/11/1');
+    let t1 = new Date(); console.log('设定日期 begin', t1);
     await page.frame({name: 'TargetContent'}).click('input[id="DERIVED_ABS_SS_SRCH_BTN"]');
-    let t1 = new Date(); console.log(`设定日期                `, t1);
-    await page.waitForEvent('requestfinished', { timeout:621000});
-    let t2 = new Date(); console.log(`设定日期 requestfinished`, t2, t2-t1);
+    await page.waitForLoadState('networkidle', { timeout:621000 });
+    let t2 = new Date(); console.log('设定日期 end  ', t2, t2-t1);
 
-    await page.frame({name: 'TargetContent'}).click('a[id="GP_ABSHISTSS_VW$hviewall$0"]', {timeout:1500}).then( async ()=>{
+    await page.frame({name: 'TargetContent'}).click('a[id="GP_ABSHISTSS_VW$hviewall$0"]', {timeout:1500}).then( async () => {
       console.log("Clicked 全部查看 button.");
-      await page.waitForEvent('requestfinished');
-      await idle(1000); //NOTE: Just don't know exactly when it is done.
-    }, ()=>{
+      await page.waitForLoadState('networkidle');
+      await page.waitForTimeout(500);
+      while (true) {
+        const cstr = await page.frame({name: 'TargetContent'}).locator('span.PSGRIDCOUNTER').innerText();
+        const d3 = /(.*?)-(.*?)\/(.*)/.exec(cstr); // 1-10/15 or 1/1
+        const nAbsRows = await page.frame({name: 'TargetContent'}).locator("table.PSLEVEL1GRID tr").count();
+        assert(d3[3] == nAbsRows-1, 'table not ready? d3=%s rows=%d', d3[3], nAbsRows-1);
+        if ( d3[3] == nAbsRows-1 ) {
+          break;
+        }
+        await page.waitForTimeout(500);
+      }
+    }, () => {
       console.log("No 全部查看 button.");
     });
-    
+
+    const counters = await page.frame({name: 'TargetContent'}).$('span.PSGRIDCOUNTER', {strict: true});
+    const nAbsRows = await page.frame({name: 'TargetContent'}).locator("table.PSLEVEL1GRID tr").count();
+    if ( counters ) {
+      const cstr = await counters.innerText();
+      const d3 = /.*?\/(.*)/.exec(cstr); // 1-10/15 or 1/1
+      assert(d3[1]==nAbsRows-1, `table not ready? counter=${d3[1]} rows=${nAbsRows-1}`);
+    } else {
+      assert(nAbsRows===0, 'weird');
+    }
+
     let begin = await page.frame({name: 'TargetContent'}).getAttribute('input[id="DERIVED_ABS_SS_BGN_DT"]', 'value');
     let end = await page.frame({name: 'TargetContent'}).getAttribute('input[id="DERIVED_ABS_SS_END_DT"]', 'value');
-    const nAbsRows = await page.frame({name: 'TargetContent'}).locator("table.PSLEVEL1GRID tr").count();
-    console.log(`From ${begin} till ${end} ${employeeName}有${nAbsRows-1}個请假记录`);
+    console.log(`From ${begin} till ${end} ${employeeName}有${nAbsRows?nAbsRows-1:0}個请假记录`);
 
     // Get details of 请假记录
     for (let r=2; r <= nAbsRows; r++) {
     //for (let r=2; r <= nAbsRows && r<=4; r++) {//DEBUG: Only get 3 of them.
-      const tbl = await page.frame({name: 'TargetContent'}).locator(`table.PSLEVEL1GRID tr:nth-child(${r}) td`).elementHandles();
-      const 假别名称 = await tbl[0].innerText();  // 特休假
-      const 申请人 = await tbl[5].innerText();    // 员工申请
+      const tds = await page.frame({name: 'TargetContent'}).locator(`table.PSLEVEL1GRID tr:nth-child(${r}) td`).elementHandles();
+      const 假别名称 = await tds[0].innerText();  // 特休假
+      const 状态 = await tds[1].innerText();      // 已批准
+      const 开始日期 = await tds[2].innerText();  // 2021/09/30
+      const 结束日期 = await tds[3].innerText();  // 2021/09/30
+      const 总计时数0 = await tds[4].innerText(); // 8 小时
+      let 总计时数 = '???';
+      const tmpHr = /(.*?) 小时/.exec(总计时数0);
+      if ( tmpHr ) {
+        总计时数 = tmpHr[1];
+      }
+      const 申请人 = await tds[5].innerText();    // 员工申请
 
-    //  const 申请人 = await page.frame({name: 'TargetContent'}).innerText(`table.PSLEVEL1GRID tr:nth-child(${r}) td:nth-child(6)`);
-    //  await page.frame({name: 'TargetContent'}).click(`table.PSLEVEL1GRID tr:nth-child(${r}) td:nth-child(1) a`);
-      await tbl[0].$('a').then(a => { return a.click() });
+      await page.frame({name: 'TargetContent'}).click(`table.PSLEVEL1GRID tr:nth-child(${r}) td:nth-child(1) a`);
 
       // Get the desired iframe name
       const frnm = await new Promise(function(resolve, reject) {
@@ -90,30 +117,18 @@ const keypress = () => {
       });
 
       const who = await page.frame({name: frnm}).innerText(`#PERSON_NAME_NAME`);  // 张翀 (JACK C ZHANG)
-      const 假别名称 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_PIN_TAKE_NUM`);  // 特休假
-      const 开始日期 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_BGN_DT`);        // 2021/09/13
+      const 假别名称1 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_PIN_TAKE_NUM`); // 特休假
+      const 开始日期1 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_BGN_DT`);       // 2021/09/13
       const 开始时间 = await page.frame({name: frnm}).innerText(`#Z_DERIVED_ABS_S_START_TIME`);   // 08:20
-      const 结束日期 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_END_DT`);        // 2021/09/14
+      const 结束日期1 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_END_DT`);       // 2021/09/14
       const 结束时间 = await page.frame({name: frnm}).innerText(`#Z_DERIVED_ABS_S_END_TIME`);     // 17:20
-      let 总计时数 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_DURATION_ABS`);    // 16.0
-      
-      if (总计时数.trim()==="") {
-        const ele = await page.frame({name: frnm}).$(`#DERIVED_ABS_SS_BEGIN_DAY_HRS`);
-        if ( ele ) {
-          总计时数 = await ele.innerText()
-        }
-        // e.g. (38妇女节)
-        //总计时数 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_BEGIN_DAY_HRS`);     // 4.0 开始日时数
-      }
+      assert(employeeName===who, 'who');
+      assert(假别名称===假别名称1, '假别名称');
+      assert(开始日期===开始日期1, '开始日期');
+      assert(结束日期===结束日期1, '结束日期');
 
       const 代理人 = await page.frame({name: frnm}).innerText(`#Z_PERS_SRCH_DEP_NAME_DISPLAY`);   // 庞美静 (TINA MJ PANG)
       const 理由 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_COMMENTS`);          // 家中有事
-      let 状态;
-      try {
-        状态 = await page.frame({name: frnm}).innerText(`#DERIVED_ABS_SS_WF_STATUS`, {timeout:100});         // 已批准
-      } catch {
-        状态 = await page.frame({name: frnm}).innerText(`#WORK_FLOW_STATUS`);
-      }
 
       // Get 签核历程
       let t1 = "" // Applicant time
@@ -127,7 +142,7 @@ const keypress = () => {
         t3 = await page.frame({name: frnm}).innerText("table[id='tdgbrZ_GP_ABS_SS_STA$0'] tr:nth-child(3) td:nth-child(4)");
       }
 
-      fo.write(`${申请人}, ${did}, ${eid}, ${employeeName}, ${假别名称}, ${开始日期}, ${开始时间}, ${结束日期}, ${结束时间}, ${总计时数}, ${代理人}, ${理由}, ${状态}, ${t1}, ${t2}, ${t3}\r\n`);
+      fo.write(`${申请人},${did},${eid},${employeeName},${假别名称},${开始日期},${开始时间},${结束日期},${结束时间},${总计时数},${代理人.trim()},${理由.trim()},${状态},${t1},${t2},${t3}\r\n`);
       await page.frame({name: frnm}).click(`a[id="DERIVED_ABS_SS_LINK"]`);  // 返回请假纪录
       await page.waitForEvent('requestfinished');
     }
@@ -152,13 +167,13 @@ const keypress = () => {
     subs.push({name:name, eid:eid, did:did, mgr:mgr});
   }
 
-  //for (let row=1; row < nRows; row++) {
-  for (let row of [3,5]) {//DEBUG
+  for (let row=1; row < nRows; row++) {
+  //for (let row of [3,5]) {//DEBUG
     //console.log(`Going into row ${row} ${subs[row-1].name}`);
+    let t1 = new Date(); console.log(`${subs[row-1].name} 选择 begin`, t1);
     await page.frame({name: 'TargetContent'}).click(`table.PSLEVEL1GRID tr:nth-child(${row+1}) td:nth-child(1) input`);  // input:has-text("选择")
-    let t1 = new Date(); console.log(`${subs[row-1].name} 选择 clicked        `, t1);
     await page.waitForEvent('requestfinished');
-    let t2 = new Date(); console.log(`${subs[row-1].name} 选择 requestfinished`, t2, t2-t1);
+    let t2 = new Date(); console.log(`${subs[row-1].name} 选择 end  `, t2, t2-t1);
     await GetLeaveRecords(subs[row-1].did, subs[row-1].eid, subs[row-1].name);
     await page.frame({name: 'TargetContent'}).click('a[id="DERIVED_ABS_SS_BACK"]'); //click('text=返回到“直接报告者”');
 
@@ -166,15 +181,16 @@ const keypress = () => {
       let l2Cnt = null;  // 直接下属的下属人数
       let l2Idx = null;
       do {
+        t1 = new Date(); console.log(`${subs[row-1].name} 展开 begin`, t1);
         await page.frame({name: 'TargetContent'}).click(`table.PSLEVEL1GRID tr:nth-child(${row+1}) td:nth-child(2) a`); // 展开
-        t1 = new Date(); console.log(`${subs[row-1].name} 展开 clicked        `, t1);
         await page.waitForEvent('requestfinished');
-        t2 = new Date(); console.log(`${subs[row-1].name} 展开 requestfinished`, t2, t2-t1);
+        t2 = new Date(); console.log(`${subs[row-1].name} 展开 end  `, t2, t2-t1);
         const nRows2 = await page.frame({name: 'TargetContent'}).locator("table.PSLEVEL1GRID tr").count();
         const tmpCnt = nRows2 - nRows;
         if ( l2Cnt === null ) {
           l2Cnt = tmpCnt;
           l2Idx = 1;
+          //l2Idx = 3;  //DEBUG
           console.log(`${subs[row-1].name} 有 ${l2Cnt} 個下屬`);
           nTotalSubs += l2Cnt;
         } else {
@@ -185,12 +201,12 @@ const keypress = () => {
         let eid  = await tbl[2].innerText();
         let did  = await tbl[4].innerText();
         await page.frame({name: 'TargetContent'}).click(`table.PSLEVEL1GRID tr:nth-child(${row+1+l2Idx}) td:nth-child(1) input:has-text("选择")`);
-        t1 = new Date(); console.log(`${name} 选择 clicked        `, t1);
+        t1 = new Date(); console.log(`${name} 选择 begin`, t1);
         await page.waitForEvent('requestfinished');
-        t2 = new Date(); console.log(`${name} 选择 requestfinished`, t2, t2-t1);
+        t2 = new Date(); console.log(`${name} 选择 end  `, t2, t2-t1);
         await GetLeaveRecords(did, eid, name);
         await page.frame({name: 'TargetContent'}).click('a[id="DERIVED_ABS_SS_BACK"]'); //click('text=返回到“直接报告者”');
-        //if ( l2Idx === 1 ) l2Idx = l2Cnt-1;  //DEBUG
+        //if ( l2Idx === 3 ) l2Idx = l2Cnt-1;  //DEBUG
         l2Idx ++;
       } while (l2Idx <= l2Cnt);
     }
